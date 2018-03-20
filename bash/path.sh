9c3f6a8c5c1cd34fe623d139c20fd72030f8ea10
Johan Öbrink <johan.obrink@gmail.com>
2018-03-20 09:22:46 +0100

    Apparently moved the Android SDK
    Probably totally brilliant

--- bash/path.sh
+++ bash/path.sh
@@ -6,7 +6,7 @@ export PATH=~/.bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/share/npm/bin:$PATH
 # Android
-export ANDROID_HOME=/Users/jok/Library/Android/sdk
+export ANDROID_HOME=~/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 # ConnectIQ
 export PATH=$PATH:/usr/local/connectiq
